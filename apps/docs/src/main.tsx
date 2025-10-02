9364332d648afd48c05259d7d35db3a47275f466
Alex M. Norris <alexmnorris.dev@gmail.com>
2025-10-02 20:03:39 +0100

    feat: code review comments

--- apps/docs/src/main.tsx
+++ apps/docs/src/main.tsx
@@ -1,12 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { AmnCounter } from "@repo/design-system";
 
-const App = () => (
-  <div>
-    <div className="card">
-      <AmnCounter />
-    </div>
-  </div>
-);
+const App = () => <div>Hello world</div>;
 
 createRoot(document.getElementById("app")!).render(<App />);
--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
-import { resolve } from "node:path";
+import dts from "unplugin-dts/vite";
 
 export default defineConfig({
   build: {
     outDir: "dist",
     cssMinify: false,
-    minify: true,
     sourcemap: false,
-    lib: {
-      entry: resolve(__dirname, "index.ts"),
-      name: "index",
-      formats: ["es"],
-      fileName: () => "index.mjs",
-    },
   },
-  plugins: [],
+  plugins: [dts()],
 });
