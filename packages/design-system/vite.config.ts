ef25c8cc791c5b35590a983e914654473bc95924
Alex M. Norris <alexmnorris.dev@gmail.com>
2025-10-03 19:05:36 +0100

    feat: more changes

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import dts from "unplugin-dts/vite";
 
 export default defineConfig({
   build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
     outDir: "dist",
     cssMinify: false,
     sourcemap: false,
